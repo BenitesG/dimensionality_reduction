8a92ba17c3926ba037e32e7f6c0867ed5318c1ea
Gustavo Oliveira Benites <super.fatalite@gmail.com>
2024-12-22 05:02:03 -0300

    binarizing

--- desafio.py
+++ desafio.py
@@ -10,13 +10,19 @@ image_rgb = np.array(image)
 image_gray = image_gray = 0.2989 * image_rgb[:, :, 0] + 0.5870 * image_rgb[:, :, 1] + 0.1140 * image_rgb[:, :, 2]  
 image_array = np.array(image_gray, dtype=float)
 
+# Set the threshold to binarization the image
+threshold = 128
+binary_image = np.where(image_array > threshold, 255, 0)
 
-# Visualization of all the images (original and grey)
+# Visualization of all the images (original, grey and binarized)
 plt.figure(figsize=(15, 5))
-plt.subplot(1, 2, 1)
+plt.subplot(1, 3, 1)
 plt.imshow(image)
 plt.title("Original")
-plt.subplot(1, 2, 2)
+plt.subplot(1, 3, 2)
 plt.imshow(image_array, cmap="gray")
 plt.title("Grey Scale")
+plt.subplot(1, 3, 3)
+plt.imshow(binary_image, cmap="gray")
+plt.title("Binarized")
 plt.show()
